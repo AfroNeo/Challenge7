9c6ed1958c96efaf40408e0b09d6c8442c7c02c6
Matthew <mbrooks9622@gmail.com>
2023-01-20 15:59:52 +0000

    live time set interval

--- script.js
+++ script.js
@@ -8,28 +8,24 @@ $("#currentDay").text(calendar.format('dddd, MMMM Do, YYYY'));
 let currentDay = document.querySelector("#currentDay");
 
 //The current time:
-let time = moment();
+setInterval(function(){let time = moment();
 $("#currentTime").text(time.format('hh:mm:ss'));
+}
+,1000)
+
+
+localStoarge.setItems
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
 
 
 
-// I want to plug this into a bootstrap table / calendar. Or not? Double check the read me demo!
-$moment("#currentDay").calendar({
-    sameDay: '[Today]',
-    nextDay: '[Tomorrow]',
-    nextWeek: 'dddd',
-    lastDay: '[Yesterday]',
-    lastWeek: '[Last] dddd',
-    sameElse: 'DD/MM/YYYY'
-});
-
-moment().calendar({
-  sameDay: function (now) {
-    if (this.isBefore(now)) {
-      return '[Will Happen Today]';
-    } else {
-      return '[Happened Today]';
-    }
-    /* ... */
-  }
-});
+// moment().calendar({
+//   sameDay: function (now) {
+//     if (this.isBefore(now)) {
+//       return '[Will Happen Today]';
+//     } else {
+//       return '[Happened Today]';
+//     }
+//     /* ... */
+//   }
+// });
