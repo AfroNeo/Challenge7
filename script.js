23371038d7b6d6fd10211b9983c17963e5f04166
Matthew <mbrooks9622@gmail.com>
2023-01-24 02:31:28 +0000

    textarea format

--- script.js
+++ script.js
@@ -22,31 +22,58 @@ $("#currentTime").text(time.format('hh:mm:ss'));
 
 //LOCAL STORAGE
 //my localStorage funtion, call and listenter
+//................................................................
 
-const input = document.querySelector("input"),
-      h2 = document.querySelector("h2");
+// const input = document.querySelector("input"),
+//       h2 = document.querySelector("h2");
 
-h2.innerHTML = localStorage.getItem("value");
+// h2.innerHTML = localStorage.getItem("value");
 
-input.addEventListener("keyup", display);
+// input.addEventListener("keyup", display);
 
-//The Function
-function display() {
-  //error here:
-  // localStoarge.setItem("value", input.value);
-  console.log(localStorage.getItem("value"));
+// //The Function
+// function display() {
+//   //error here:
+//   // localStoarge.setItem("value", input.value);
+//   console.log(localStorage.getItem("value"));
 
-  h2.innerHTML = localStorage.getItem("value");
-}
+//   h2.innerHTML = localStorage.getItem("value");
+// }
 
-function removeItem(){
-  localStoarge.removeItem('keyName');
-}
+// function removeItem(){
+//   localStoarge.removeItem('keyName');
+// }
 
-function clear(){
-localStorage.clear('keyName');
-}
+// function clear(){
+// localStorage.clear('keyName');
+// }
 
+//............................................................................//
+//local storage attempt 2. this time using textarea
+
+localStorage.setItem("9am")
+
+localStorage.getItem("9am")
+
+$("textarea")
+$("textarea").text(localStorage.getItem("9am"))
+
+
+
+
+
+
+
+
+
+
+//use moments to print out the format. Do I need to apply .past to it.
+// need to start at 9am for a 24hr.
+//set the textarea $("textarea").addClass("future")
+
+// Then find out what button was clicked, as well as the one next to it.
+//jQWuery
+// use fonmt awesome 5.8 for yoru icons as the add button example.
 
 
 
